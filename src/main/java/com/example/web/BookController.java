7588764e25f231122575d1c92d1400417e2a5db1
Ольга Ворожейкина < >
2016-12-08 19:01:08 +0200

    - Adding RESTful services

--- src/main/java/com/example/web/BookController.java
+++ src/main/java/com/example/web/BookController.java
@@ -1,15 +1,21 @@
 package com.example.web;
 
+import java.util.List;
+
+import javax.persistence.ManyToOne;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.domain.Book;
 import com.example.domain.BookRepository;
 import com.example.domain.CategoryRepository;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Controller
 public class BookController {
@@ -48,5 +54,19 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";        
     }
+	
+	// RESTful service to get all books
+	@ManyToOne
+	@JsonIgnore
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);
+    } 
 
 }
