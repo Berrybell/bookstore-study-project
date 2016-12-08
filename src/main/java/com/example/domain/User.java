e2faf32360e6394f23b06de788794e9458beddb3
Ольга Ворожейкина < >
2016-12-09 00:36:54 +0200

    - Updated user entities

--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -16,6 +16,9 @@ public class User {
 
     @Column(name = "password", nullable = false)
     private String passwordHash;
+    
+    @Column(name = "email", nullable = false)
+    private String email;
 
     @Column(name = "role", nullable = false)
     private String role;
@@ -23,10 +26,11 @@ public class User {
     public User() {
     }
 
-	public User(String username, String passwordHash, String role) {
+	public User(String username, String passwordHash, String email, String role) {
 		super();
 		this.username = username;
 		this.passwordHash = passwordHash;
+		this.email = email;
 		this.role = role;
 	}
 
@@ -52,6 +56,14 @@ public class User {
 
 	public void setPasswordHash(String passwordHash) {
 		this.passwordHash = passwordHash;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getRole() {
